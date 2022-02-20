6e65d3164043119a49cec4206e6d92ba73464024
Patil0216 <98605391+Patil0216@users.noreply.github.com>
2022-02-20 16:32:38 +0530

    Add files via upload

---
+++ Print_int_double_float_value.java
@@ -0,0 +1,22 @@
+package Java.Fundamentals;
+import java.util.Scanner;
+
+public class Print_int_double_float_value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the int,float, doble values ::");
+		int i=scan.nextInt();
+		float p=scan.nextFloat();
+		double v=scan.nextDouble();
+		System.out.println(i +"\n" + p + "\n" + v + "\n");
+		
+		
+		
+		
+
+	}
+
+}
