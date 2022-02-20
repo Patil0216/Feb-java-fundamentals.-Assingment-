8c04b7c7f53144cc6018280a26a338f4f36aca9e
Patil0216 <98605391+Patil0216@users.noreply.github.com>
2022-02-20 17:15:11 +0530

    Add files via upload

---
+++ ASCII_Value.java
@@ -0,0 +1,15 @@
+package Java.Fundamentals;
+
+import java.util.Scanner;
+
+public class ASCII_Value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a='A', b='Z';
+		System.out.println(a+"  \n" + b);
+		
+
+	}
+
+}
