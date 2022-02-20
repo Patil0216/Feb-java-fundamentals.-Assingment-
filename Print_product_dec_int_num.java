c6ef9ba4d35dd23ef652d868858b3107ae6346b2
Patil0216 <98605391+Patil0216@users.noreply.github.com>
2022-02-20 16:33:52 +0530

    Add files via upload

---
+++ Print_product_dec_int_num.java
@@ -0,0 +1,20 @@
+package Java.Fundamentals;
+import java.util.Scanner;
+
+public class Print_product_dec_int_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int i=scan.nextInt();
+		float p=scan.nextFloat();
+		float c = i*p;
+	     
+		
+		System.out.println("Product is :: "+c);
+		
+		
+
+	}
+
+}
